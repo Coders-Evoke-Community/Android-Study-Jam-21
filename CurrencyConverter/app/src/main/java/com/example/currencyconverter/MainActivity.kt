70756a863bd8008c6219d58867ff92490d940613
Yonit Chauhan <yonit833@gmail.com>
2021-12-04 16:46:15 +0530

    Added : Currency Converter Layout

---
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+### Google Study Jam Android 
+
+## Youtube Links
